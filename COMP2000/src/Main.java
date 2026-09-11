982c0bf86213d45b414a49473070ba1f24164b49
Khoi Nguyen Nguyen Tran <khoinguyennn231@gmail.com>
2026-09-11 20:03:09 +1000

    fix: randomize human and zombie spawn positions

--- COMP2000/src/Main.java
+++ COMP2000/src/Main.java
@@ -132,8 +132,8 @@ public class Main {
         // Part B: people wander until they find and consume nearby food.
         for (int index = 0; index < settings.getHumanCount(); index++) {
             world.addEntity(new Human(
-                    getSpawnCoordinate(index, 137, world.getWidth()),
-                    getSpawnCoordinate(index, 83, world.getHeight())
+                    getRandomSpawnCoordinate(world.getWidth()),
+                    getRandomSpawnCoordinate(world.getHeight())
             ));
         }
 
@@ -150,18 +150,10 @@ public class Main {
 
         for (int index = 0; index < settings.getZombieCount(); index++) {
             world.addEntity(new Zombie(
-                    getSpawnCoordinate(index, 173, world.getWidth()),
-                    getSpawnCoordinate(index, 149, world.getHeight())
+                    getRandomSpawnCoordinate(world.getWidth()),
+                    getRandomSpawnCoordinate(world.getHeight())
             ));
         }
-    }
-
-    private static double getSpawnCoordinate(
-            int index,
-            int step,
-            int dimension
-    ) {
-        return 80 + (index * step) % (dimension - 160);
     }
 
     private static double getRandomSpawnCoordinate(int dimension) {
